f9069fa517ab28f5289a0463ab6e8cebae4d25e3
Diogo Biz <diogo-biz@hotmail.com>
2022-10-01 10:22:55 -0300

    feat: add action

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Cissa Imagem e Estilo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
