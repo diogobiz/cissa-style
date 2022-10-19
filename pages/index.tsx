a69d9aaafcbf9f74b7d8f045cc23330e514f68b6
Diogo Biz <diogo-biz@hotmail.com>
2022-10-18 23:07:49 -0300

    feat: add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,39 @@
 export default function Home() {
   return (
     <main>
-      <h1>Cissa Imagem e Estilo</h1>
-      <h1>Em Breve</h1>
+      <div
+        className="mx-auto"
+        style={{
+          width: "100%",
+          height: 0,
+          paddingBottom: "133%",
+          position: "relative",
+        }}
+      >
+        <iframe
+          src="https://giphy.com/embed/kaMwwM91UCxstRfvA3"
+          width="100%"
+          height="100%"
+          style={{ position: "absolute" }}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <h1 className="text-4xl text-center font-bold mt-6">
+        Feliz aniversário!!
+      </h1>
+
+      <h1 className="text-lg text-center font-bold mt-6">
+        Seja feliz, hoje e para sempre,
+        <br /> comigo ao seu lado! <br />
+        Espero estar contigo em todos <br />
+        os seus aniversários.
+      </h1>
+
+      <h1 className="text-base text-center font-bold text-rose-400 mt-6">
+        Ps: Em breve aqui vai ser seu site.
+      </h1>
     </main>
   );
 }
